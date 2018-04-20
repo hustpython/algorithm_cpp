568462a795db8d4528e10dcdd465f994fdcaad9d
mxq <1529806383@qq.com>
2018-04-20 15:55:44 +0800

    s

--- dijkstra/main.cpp
+++ dijkstra/main.cpp
@@ -10,6 +10,18 @@ bool check(int Vexnum,int edge)
 }
 int main()
 {
+    /*
+    输入：
+    6 8
+    1 3 10
+    1 5 30
+    1 6 100
+    2 3 5
+    3 4 50
+    4 6 10
+    5 6 60
+    5 4 20
+    */
     int vexnum; int edge;
 
     cout <<"请输入图的顶点个数和边的条数:" << endl;
