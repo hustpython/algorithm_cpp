aa7c8d7bbca86dfe2b5f4f3868dc4d15b71ca207
mxq <1529806383@qq.com>
2018-05-08 13:05:20 +0800

    s

--- List/singleList.c
+++ List/singleList.c
@@ -24,11 +24,12 @@ Node * CreateNode(int val)
     }
 }
 // 尾部插入
-void Insertwithtail(Node *head,Node * & tail,int val)
+//void Insertwithtail(Node *head,Node * & tail,int val)  cpp
+void Insertwithtail(Node *head,Node * tail,int val)
 {
     Node *newNode = CreateNode(val);
     tail->next = newNode;//将新的节点插入到尾部
-    tail = tail->next;
+    //tail = tail->next; cpp
     head ->val ++;
 }
 //尾部插入
@@ -44,7 +45,7 @@ void InsertdNotail(int num,Node *head)
     L->next = newNode;//挂在原来尾节点之后   
 }  
 
-//前部插入
+//前部插入,遍历顺序与插入顺序相反
 void Inserthead(Node *head,int val)
 {
     Node *p = CreateNode(val);
@@ -55,9 +56,36 @@ void Inserthead(Node *head,int val)
 //删除节点
 void deleteByVal(Node * head,int val)
 {
-    if(head -> next == NULL)
+    if(head ->next == NULL)
     {
         printf("empty list\n");
+        return ;
+    }
+
+    Node *cur = head ->next, *pre = head;
+    while (cur)
+    {
+        if(cur->val == val)
+        {
+            break;
+        }
+        else
+        {
+            cur = cur->next;
+            pre = pre ->next;
+        }
+    }
+    pre ->next = cur ->next;
+    free(cur);
+    head ->val --;
+}
+//清空列表
+void Free(Node * head)
+{
+    for(Node * temp = head ; temp != NULL;temp = head -> next)
+    {
+        head = head -> next;
+        free(temp);
     }
 }
 int main(){
@@ -78,9 +106,12 @@ int main(){
     for(int i = 1;i<= 10;++i)
     {
          Insertwithtail(head, tail , i);
-         //tail=tail->next;//tail指向的next变了
+         tail=tail->next;//值传递，注意与引用传递的区别
+                         //tail指向的next变了,
          //Inserthead(head,i);
     }
+    //Free(head);
+    deleteByVal(head,3);
     
     Node *p = head ->next ;
     
@@ -89,7 +120,5 @@ int main(){
         printf("% d->",p->val);
         p =  p->next;
     }
-    //free(head);
-    //free(tail);
     return 0;
 }
