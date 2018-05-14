d040b3b1b98dbe139e7f41aa04c474da18ad71f2
mxq <1529806383@qq.com>
2018-05-14 23:05:44 +0800

    动态规划

---
+++ 动态规划/ladder.cpp
@@ -0,0 +1,73 @@
+# include <iostream>
+# include <map>
+using namespace std;
+// 题目描述:
+// 有20层的梯子，上楼时可以一次走一步或一次走两步。
+// 请问，一共有多少上楼方式。
+
+//动态规划 O(2^n)
+int ladderres(int num)
+{
+
+    if (num == 1)
+    {
+        return 1;
+    }
+    else if(num == 2)
+    {
+        return 2;
+    }
+    else 
+    {
+        return (ladderres(num-1)+ladderres(num-2));
+    }
+}
+int ladder_mapsearch(int num,map<int,int> &cmap)
+//备忘录算法
+//使用递归会重复计算，可以通过map将计算过的节点保存
+//在每次计算之前进行查询。
+{
+    if (num == 1)
+    {
+        return 1;
+    }
+    else if(num == 2)
+    {
+        return 2;
+    }
+    map<int,int>::iterator key = cmap.find(num);
+    if(key != cmap.end())
+    {
+        return key->second;
+    }
+    else
+    {
+        int value = ladder_mapsearch(num - 1,cmap) + ladder_mapsearch(num - 2,cmap);
+        cmap.insert(make_pair(num,value));
+        return value;
+    }
+    
+    
+}
+//方法三：从底向上
+int ladder_downtoup(int num)
+{
+    int a = 1;
+    int b = 2;
+    int temp = 0;
+    for(int i =3;i<=num;++i)
+    {
+        
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return temp;
+}
+int main()
+{
+    map<int,int> cmap;
+    //int res = ladder_mapsearch(20,cmap);
+    int res = ladder_downtoup(20);
+    cout << res << endl;
+}
--- 树/ispower2.cpp
+++ 树/ispower2.cpp
@@ -1,9 +1,40 @@
 # include <iostream>
 using namespace std;
-int main()
-{
- bool res;
- int a = 8;
- res = a & (a-1);
- cout << res << endl;
-}
+
+ 
+int log2(int value)  //非递归判断一个数是2的多少次方 
+{ 
+  int x=0; 
+  while(value>1) 
+  { 
+    value>>=1; 
+    x++; 
+  } 
+  return x; 
+} 
+int Func3(int data) 
+{  //利用了data&(data-1)每次都能移除最右边的1，移除了多少个1，就是包含了几个1 
+  int count = 0; 
+  while (data) 
+  { 
+    data = data & (data-1); 
+    count++; 
+  } 
+  return count; 
+} 
+
+int main(void) 
+{ 
+  int num; 
+  cout<<  "请输入一个整数：" << endl; 
+  cin >> num; 
+  int numofone;
+  numofone = Func3(num);
+  cout << numofone << endl;
+  if(num&(num-1))   //使用与运算判断一个数是否是2的幂次方 
+    cout<<num<<"不是2的幂次方!"<<endl; 
+  else 
+    cout<<num<<"是2的"<<log2(num)<<"次方！"<<endl;
+
+  return 0; 
+} 
