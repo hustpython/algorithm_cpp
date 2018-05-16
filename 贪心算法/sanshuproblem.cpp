7d5062b4284fdcae99c21d4d614db854ebab2d78
mxq <1529806383@qq.com>
2018-05-16 19:30:56 +0800

    add code of sanshu

--- 贪心算法/sanshuproblem.cpp
+++ 贪心算法/sanshuproblem.cpp
@@ -1,40 +1,39 @@
-// 给定n位正整数a，去掉其中任意k个数字后，剩下的数字按原次序排列组成一个新的正整数。 
-// 对于给定的n和k，设计一个算法，找出剩下数字组成的新数最少的删数方案。 
-// 输入示例：178543 4 
-// 输出：13
-
-# include <iostream>
-# include<stdio.h>
-# include<string.h>
-# include <algorithm>
-using namespace std;
-
-string a = "5934625578";
-int n = 6;
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//5934625578
+//6
+//2557
 int main()
 {
-    char a[100];
-    int n=4,i,j;
-    printf("输入数字:\n"); 
-    scanf("%s",a);  
-    printf("输入要删除的位数：\n"); 
-    scanf("%d",&n);
-    if(n==strlen(a)){ //如果删除的位数=输入的数字位数,则为0
-        printf("0");
-        return 0; 
-    } 
-
-    while(n>0) {//直到要删除的位数为0
-        i=0; //每次开始将i初始化为0，表示重新开始检测下降点 
-
-        while(i<strlen(a) && a[i]<a[i+1])
-            //在位数范围内，到了递减区间，就跳出循环
-            i++;  
-        for(j=i;j<strlen(a);j++) //后面往前挤一位，实现删除i位。
-            a[j]=a[j+1];
-        n--; 
-    }  
-    printf("%s\n",a);
+    int a, b, len, n;
+    char m[123];//输入的字符串（由于长度很长，不用int或long long int)
+    while(~scanf("%s %d", m, &b))//多组输入
+    {
+        while(b--)//共删掉b个数
+        {
+            a = 0;
+            len = strlen(m);
+            while(a<len&&m[a]<=m[a+1])//寻找第一个递减的数字
+                a++;
+            while(a<len)//找到后，将其后的字符向前移动一位（相当于删掉）
+            {
+                m[a] = m[a+1];
+                a++;
+            }
+        }//利用此方法可以使得删掉数字后剩下的数最小
+        len = strlen(m);//删完后的长度
+        n = 0;
+        while(m[n]=='0'&&n<len)//去掉可能存在‘000’这样的情况
+            n++;
+        if(n==len)//数字为零
+            printf("0\n");
+        else//输出剩余的数字
+        {
+            for(; n<len; n++)
+                printf("%c", m[n]);
+            printf("\n");
+        }
+    }
     return 0;
 }
